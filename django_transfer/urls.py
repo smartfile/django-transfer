eb4456b752313383a573bacfc102db9149ee1854
Ben Timby <btimby@smartfile.com>
2017-06-16 10:01:09 -0400

    Fix URL patterns for different Django versions.

--- django_transfer/urls.py
+++ django_transfer/urls.py
@@ -1,13 +1,18 @@
 from __future__ import unicode_literals
 
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
+
+    def patterns(*args):
+        return args
+
 except ImportError:
     from django.conf.urls.defaults import patterns, url
 
+from django_transfer.views import download, upload
+
 
 urlpatterns = patterns(
-    '',
-    url(r'^download/.*$', 'django_transfer.views.download', name='download'),
-    url(r'^upload/$', 'django_transfer.views.upload', name='upload'),
+    url(r'^download/.*$', download, name='download'),
+    url(r'^upload/$', upload, name='upload'),
 )
